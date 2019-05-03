810e1ae03a04a6b94d722817901243ff6958dc0c
Magda Bula <magdalena.bula@gmail.com>
2019-05-03 19:57:37 +0200

    Add Booking class and initBooking

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import { Product } from './components/Product.js';
 import { Cart } from './components/Cart.js';
-import { select, settings } from './settings.js';
+import { Booking } from './components/Booking.js';
+import { select, settings, classNames } from './settings.js';
 
 const app = {
   initMenu: function() {
@@ -37,10 +38,60 @@ const app = {
     });
   },
 
+  initPages() {
+    const thisApp = this;
+
+    thisApp.pages = Array.from(document.querySelector(select.containerOf.pages).children);
+    thisApp.navLinks = Array.from(document.querySelectorAll(select.nav.links));
+    //console.log(thisApp.navLinks);
+    //thisApp.activatePage(thisApp.pages[0].id);
+    let pagesMatchingHash = [];
+    if (window.location.hash.lenght > 2) {
+      const idFromHash = window.location.hash.replace('#/', '');
+      pagesMatchingHash = thisApp.pages.filter(function(page) {
+        return page.id == idFromHash;
+      });
+    }
+
+    thisApp.activatePage(pagesMatchingHash.lenght ? pagesMatchingHash[0].id : thisApp.pages[0].id);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function() {
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+      });
+    }
+  },
+
+  activatePage(pageId) {
+    const thisApp = this;
+
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(classNames.nav.active, link.getAttribute('href') == '#' + pageId);
+    }
+
+    for (let page of thisApp.pages) {
+      page.classList.toggle(classNames.nav.active, page.getAttribute('id') == pageId);
+    }
+    window.location.hash = '#/' + pageId;
+  },
+
+  initBooking() {
+    const thisApp = this;
+    const bookingWidget = document.querySelector(select.containerOf.booking);
+    //console.log(bookingWidget);
+    thisApp.booking = new Booking(bookingWidget);
+  },
+
   init: function() {
     const thisApp = this;
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
+    thisApp.initBooking();
   },
 };
 
